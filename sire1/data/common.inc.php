88f0dc3de329028d497cf980b6e15fbd30253026
sangesha <307475628@qq.com>
2018-08-03 16:08:31 +0800

    init

---
+++ sire1/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '47.106.92.252';
+$cfg_dbname = 'qiansheng1';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'Aa20180522';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
